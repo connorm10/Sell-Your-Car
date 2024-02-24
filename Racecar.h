167e4255edfd146b05c0235880adc676feb8650e
connorm10 <92907749+connorm10@users.noreply.github.com>
2024-02-23 19:39:00 -0500

    Update Racecar.h
    Constructor, getters and setters

--- Racecar.h
+++ Racecar.h
@@ -7,6 +7,30 @@
 #include "Car.h"
 
 class Racecar : public Car {
+private:
+    int topSpeed;
+    int mpg;
+    string modelName;
+    int price;
+
+public:
+    Racecar(); // Constructor
+
+    // Getters
+    int getTopSpeed();
+    int getMpg();
+    string getModelName();
+    int getPrice();
+
+    // Setters
+    void setTopSpeed(int);
+    void setMpg(int);
+    void setModelName(string);
+    void setPrice(int);
+
+    // Override the virtual sellCar method defined in Car.h, Virtual functions
+    // are member functions whose behavior can be overridden in derived classes
+    void sellCar() override;
 };
 
 
