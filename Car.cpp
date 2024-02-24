d285756e879648e5a673c25dbc02b888d948545b
connorm10 <92907749+connorm10@users.noreply.github.com>
2024-02-23 20:10:48 -0500

    Update Car.cpp
    get and set within Car.h

--- Car.cpp
+++ Car.cpp
@@ -8,3 +8,48 @@ Car::Car() {
     brand = "default";
     model = "default";
 }
+
+// Get
+
+int Car::getTopSpeed() {
+    return topSpeed;
+}
+
+int Car::getMPG() {
+    return MPG;
+}
+
+int Car::getMSRP() {
+    return MSRP;
+}
+
+string Car::getBrand() {
+    return brand;
+}
+
+
+string Car::getModel() {
+    return model;
+}
+
+// Set
+
+void Car::setTopSpeed(int val) {
+    topSpeed = val;
+}
+
+void Car::setMPG(int val) {
+    MPG = val;
+}
+
+void Car::setMSRP(int val) {
+    MSRP = val;
+}
+
+void Car::setBrand(string val) {
+    brand = val;
+}
+
+void Car::setModel(string val) {
+    model = val;
+}
