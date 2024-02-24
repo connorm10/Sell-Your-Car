b9ef389f8bae6f0c9af1ade24768e5a43f3cc857
connorm10 <92907749+connorm10@users.noreply.github.com>
2024-02-23 19:37:51 -0500

    Update Racecar.h

--- Racecar.h
+++ Racecar.h
@@ -1,1 +1,13 @@
+#ifndef M2OEP_CHMOONEY_RACECAR_H
+#define M2OEP_CHMOONEY_RACECAR_H
 
+#include "vector"
+#include "istream"
+#include "ostream"
+#include "Car.h"
+
+class Racecar : public Car {
+};
+
+
+#endif //M2OEP_CHMOONEY_RACECAR_H
