367bc865a12aa0deea4dbbf035ab7c11ac664e99
connorm10 <92907749+connorm10@users.noreply.github.com>
2024-02-25 11:10:18 -0500

    Update Racecar.cpp
    completed on 2/24 but forgot to hit commit and fell asleep lol, added getters and setters, added beginning of function for calculating an estimated sale price for a Racecar

--- Racecar.cpp
+++ Racecar.cpp
@@ -1,1 +1,47 @@
+#include "Racecar.h"
+#include <iostream>
 
+Racecar::Racecar() : Car(), avgPlacement(0), salary(0), driverName("Jane Doe") {
+}
+
+int Racecar::getAvgPlacement() {
+    return avgPlacement;
+}
+
+int Racecar::getSalary() {
+    return salary;
+}
+
+string Racecar::getDriverName() {
+    return driverName;
+}
+
+int Racecar::getNumWins() {
+    return numWins;
+}
+
+void Racecar::setAvgPlacement(int val) {
+    if (val > 0) {
+        avgPlacement = val;
+    }
+}
+
+void Racecar::setSalary(int val) {
+    if (val > 0){
+        salary = val;
+    }
+}
+
+void Racecar::setDriverName(string val) {
+    driverName = val;
+}
+
+void Racecar::setPilotWage(int val) {
+    if (val > 0){
+        numWins = val;
+    }
+}
+
+// Function to calculate an estimated sale price for the car
+void Racecar::sellCar() {
+}
