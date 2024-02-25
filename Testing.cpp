b2a2e168880a88682c92d6c27e631d9f17efddbc
connorm10 <92907749+connorm10@users.noreply.github.com>
2024-02-25 14:27:38 -0500

    Update Testing.cpp
    Added different testing cases to ensure different parts of program are working

--- Testing.cpp
+++ Testing.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include "Racecar.h"
 
+bool testRacecar();
+
+int Main(){
+    if (testRacecar()){
+        cout << "Passed all test cases!!! :)" << endl;
+    }
+    return 0;
+}
+
+bool testRacecar() {
+    bool test_case_result = true; // Starts as passed
+    Racecar racecar; // Create racecar to test
+
+  // Default Constructor Test Case (getDriverName)
+    if (racecar.getDriverName() != "Jane Doe"){
+        cout << "FAILED IN CREATING DEFAULT CONSTRUCTOR CASE!!!" << endl;
+        test_case_result = false;
+    }
+  
+  // setSalary Test Case (int)
+    racecar.setSalary(1000000);
+    if (racecar.getSalary() != 1000000) {
+        cout << "FAILED SALARY TEST CASE!!!" << endl;
+        test_case_result = false;
+    }
+
+  // setDriverName test case (string)
+    racecar.setDriverName("Max Verstappen");
+    if (racecar.getDriverName() != "Max Verstappen"){
+        cout << "Failed TO SET DRIVER NAME!!!" << endl;
+        test_case_result = false;
+    }
+  
+    return test_case_result; // True = passes test case, False = fails test case
+}
