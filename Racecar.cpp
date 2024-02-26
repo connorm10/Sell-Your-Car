5edd577a55acc7d62890590ad4f7b6216959578d
Connor Mooney <mooney.connor@gmail.com>
2024-02-25 20:54:46 -0500

    added funtion to create estimated sale price and flyer with summarized info that is input my the user

--- Racecar.cpp
+++ Racecar.cpp
@@ -36,12 +36,138 @@ void Racecar::setDriverName(string val) {
     driverName = val;
 }
 
-void Racecar::setPilotWage(int val) {
+void Racecar::setNumWins(int val) {
     if (val > 0){
         numWins = val;
     }
 }
 
 // Function to calculate an estimated sale price for the car
+// Prints info for car at end
 void Racecar::sellCar() {
+
+    string inputStr;
+    int inputInt;
+    bool error = false;
+
+    cout << "Post your racecar for sale" << endl;
+
+    do{
+        cout << "Enter brand of racecar:";
+        cin >> inputStr;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid string input" << endl;
+            error = true;
+        }
+    }while(error);
+    setBrand(inputStr);
+
+    do{
+        cout << "Enter model of racecar:";
+        cin >> inputStr;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid string input" << endl;
+            error = true;
+        }
+    }while(error);
+    setModel(inputStr);
+
+    do {
+        cout << "Enter top speed of racecar:";
+        cin >> inputInt;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid integer input" << endl;
+            error = true;
+        }
+    }while(error);
+    setTopSpeed(inputInt);
+
+    do {
+        cout << "Enter MPG of the racecar:";
+        cin >> inputInt;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid integer input" << endl;
+            error = true;
+        }
+    }while(error);
+    setMPG(inputInt);
+
+    do{
+        cout << "Enter MSRP of racecar:";
+        cin >> inputInt;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid integer input" << endl;
+            error = true;
+        }
+    }while(error);
+    setMSRP(inputInt);
+
+    do{
+        cout << "Enter average podium placement of racecar:";
+        cin >> inputInt;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid integer input" << endl;
+            error = true;
+        }
+    }while(error);
+    setAvgPlacement(inputInt);
+
+    do{
+        cout << "Enter the name of the racecar's driver:";
+        cin >> inputStr;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid string input" << endl;
+            error = true;
+        }
+    }while(error);
+    setDriverName(inputStr);
+
+    do{
+        cout << "Enter the salary of the driver :";
+        cin >> inputInt;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid integer input" << endl;
+            error = true;
+        }
+    }while(error);
+    setSalary(inputInt);
+
+
+    do{
+        cout << "Enter the number of wins the racecar has:";
+        cin >> inputInt;
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "Enter a valid integer input" << endl;
+            error = true;
+        }
+    }while(error);
+    setNumWins(inputInt);
+
+    int estimatedSellingPrice = ((getNumWins() * (100000)) + getMSRP());
+
+    cout << "----------Racecar For Sale----------" << endl;
+    cout << "Racecar: " << getBrand() << " " << getModel() << endl;
+    cout << "\tDriver:" << getDriverName() << endl;
+    cout << "\tTop Speed:" << getTopSpeed() << endl;
+    cout << "\tAverage Finishing Place:" << getAvgPlacement() << endl;
+    cout << "\tNumber of Wins:" << getNumWins() << endl;
+    cout << "\tSuggested Sale Price:" << estimatedSellingPrice << endl;
 }
